411c7f026dbe9caa84f737bc691021a1c2626655
nekoumei <nekoumei@gmail.com>
2018-04-25 23:42:55 +0900

    add py

---
+++ src/arg_predict.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import gc
+import datetime
+from pathlib import Path
+
+PROJECT_ROOT = Path('.') / '..'
+DATA_DIR = PROJECT_ROOT / 'data'
+BAGGING_DIR = DATA_DIR / 'bagging'
+
+csvs = [predicted for predicted in BAGGING_DIR.glob('*.csv')]
+print(BAGGING_DIR.resolve())
+merge = pd.DataFrame()
+for i, predicted_csv in enumerate(csvs):
+    pred_df = pd.read_csv(predicted_csv)
+    if i == 0:
+        merge['click_id'] = pred_df['click_id']
+    merge = pd.concat([merge, pred_df['is_attributed']], axis=1)
+    print(merge.head())
+
+calc_df = merge.drop('click_id', axis=1)
+calc_df = calc_df.mean(axis=1)
+merge = pd.concat([merge['click_id'], calc_df], axis=1)
+merge.columns = ['click_id', 'is_attributed']
+now = datetime.datetime.now()
+merge.to_csv('../data/submission_lgb_bugged_{0:%Y%m%d_%H%M%S}.csv.gz'.format(now),
+             compression='gzip',
+             index=False
+             )
