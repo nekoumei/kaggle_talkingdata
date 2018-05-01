e88210977ea9963d64f707ca90b6d397ac1cac29
nekoumei <nekoumei@gmail.com>
2018-05-01 17:45:00 +0900

    dummies

--- src/add_pca.py
+++ src/add_pca.py
@@ -158,6 +158,7 @@ if __name__ == '__main__':
     from sklearn.decomposition import PCA
     pca = PCA(n_components=10)
     bef_pca = merge[not_categorical]
+    bef_pca.fillna(0, inplace=True)
     aft_pca = pca.fit_transform(bef_pca.as_matrix())
     print('[{}]Finished:PCA'.format(get_now()))
     merge = pd.concat([merge, aft_pca], axis=1, ignore_index=True)
