21b806ca69d609a4611e6e99b11f8a77702dfad7
nekoumei <nekoumei@gmail.com>
2018-05-07 21:08:30 +0900

    updated

--- src/arg_predict.py
+++ src/arg_predict.py
@@ -8,7 +8,12 @@ PROJECT_ROOT = Path('.') / '..'
 DATA_DIR = PROJECT_ROOT / 'data'
 BAGGING_DIR = DATA_DIR / 'bagging_20180426_002417'
 
-csvs = [predicted for predicted in BAGGING_DIR.glob('*.csv.gz')]
+# csvs = [predicted for predicted in BAGGING_DIR.glob('*.csv.gz')]
+csvs = [
+    DATA_DIR / 'sub_bai_6.csv.gz',
+    DATA_DIR / 'sub_lgb_semis_20180501_124039.csv.gz'
+    DATA_DIR / 'submission_lgb_20180428_160430.csv.gz'
+]
 print(BAGGING_DIR.resolve())
 merge = pd.DataFrame()
 for i, predicted_csv in enumerate(csvs):
